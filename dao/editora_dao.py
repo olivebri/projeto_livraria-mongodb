e67d2856c23e62e283f5c75ae6af320a5e5a61bd
unknown <alx.brian@gmail.com>
2023-05-31 19:32:50 -0300

    Versao Final

--- dao/editora_dao.py
+++ dao/editora_dao.py
@@ -1,39 +1,64 @@
 from model.editora import Editora
+from database.client_factory import ClientFactory
+from bson import ObjectId
+
 
 class EditoraDAO:
 
     def __init__(self):
+        self.__client_factory: ClientFactory = ClientFactory()
         self.__editoras: list[Editora] = list()
 
     def listar(self) -> list[Editora]:
-        return self.__editoras
+        editoras = list()
+        client = self.__client_factory.get_client()
+        db = client.livraria
+
+        for documento in db.editoras.find():
+            edt = Editora(documento['nome'],documento['endereco'], documento['telefone'])
+            edt.id = documento['_id']
+            editoras.append(edt)
+        
+        client.close
+        return editoras
 
     def adicionar(self, editora: Editora) -> None:
-        self.__editoras.append(editora)
+        client = self.__client_factory.get_client()
+        db = client.livraria
+        db.editoras.insert_one({
+            'nome': editora.nome,
+            'telefone': editora.telefone,
+            'endereco': editora.endereco
+        })
 
-    def remover(self, editora_id: int) -> bool:
-        encontrado = False
-        for e in self.__editoras:
-            if (e.id == editora_id):
-                index = self.__editoras.index(e)
-                self.__editoras.pop(index)
-                encontrado = True
-                break
-        return encontrado
+        client.close()
+
+
+    def remover(self, editora_id: str) -> bool:
+        client = self.__client_factory.get_client()
+        db = client.livraria
+
+        resultado = db.editoras.delete_one(
+            {'_id': ObjectId(editora_id)}
+        )
+
+        if (resultado.deleted_count == 1):
+            return True
+        return False
+
 
     def buscar_por_id(self, editora_id) -> Editora:
         edt = None
-        for e in self.__editoras:
-            if (e.id == editora_id):
-                edt = e
-                break
+        client = self.__client_factory.get_client()
+        db = client.livraria
+        resultado = db.editoras.find_one({
+            '_id': ObjectId(editora_id)
+        })
+        
+        if (resultado):
+            edt = Editora(resultado['nome'], resultado['endereco'], resultado['telefone'])
+            edt.id = resultado['id']
+        
+        client.close()
+
         return edt
-    
-    def ultimo_id(self) -> int:
-        index = len(self.__editoras) -1
-        if (index == -1):
-            id = 0
-        else:
-            id = self.__editoras[index].id
-        return id
-    
