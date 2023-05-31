7dcb09c8122660f8189bec77ba8590fe9e2a6577
olivebri <133821561+olivebri@users.noreply.github.com>
2023-05-30 21:48:25 -0300

    Update client_factory.py

--- database/client_factory.py
+++ database/client_factory.py
@@ -4,5 +4,5 @@ from pymongo.server_api import ServerApi
 class ClientFactory:
     
     def get_client(self):
-        return MongoClient("mongodb+srv://olivebri:sa3RY8vAWn5g6g6w@cluster0.jovjuzl.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+        return MongoClient("mongodb+srv://<user>:<password>@cluster0.jovjuzl.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 
